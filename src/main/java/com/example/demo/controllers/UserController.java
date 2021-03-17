877f2d7e3f3b39ae27649aa0b96648c5dd08c2ae
Clarence <clarencelindh@gmail.com>
2021-03-17 10:09:58 +0100

    Program branch test

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/rest")
 public class UserController {
-  /*
+  /* asd
       C - @PostMapping
       R - @GetMapping
       U - @PutMapping
