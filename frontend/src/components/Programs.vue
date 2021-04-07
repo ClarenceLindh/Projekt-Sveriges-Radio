0c1c8a68a7dda54e5183bb66b92610a94daf153a
Clarence <clarencelindh@gmail.com>
2021-04-07 14:10:12 +0200

    Frontend started

---
+++ frontend/src/components/Programs.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>Programs</h1>
+  
+</template>
+
+<script>
+export default {
+    name: "Programs",
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Favorites.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="favorites">
+    <h1>Favorites</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Favorites",
+  components: {
+    
+  },
+};
+</script>
