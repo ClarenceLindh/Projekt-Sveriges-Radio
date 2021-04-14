9c0d5398e11b8c8a5cdbabf0ebb2a76df7f0e51a
Martin Jönsson <mr_tines_j@hotmail.com>
2021-04-14 16:30:53 +0200

    getting closer

--- frontend/src/components/Social.vue
+++ frontend/src/components/Social.vue
@@ -39,4 +39,22 @@ export default {
         overflow: auto;
         max-height: 67vh;
     }
+
+    #socialList::-webkit-scrollbar-track{
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+	border-radius: 10px;
+	background-color: rgba(60, 55, 65, .3);
+    }
+
+    #socialList::-webkit-scrollbar{
+        width: 12px;
+        background-color: rgba(0, 0, 0, 0);
+    }
+
+    #socialList::-webkit-scrollbar-thumb{
+        border-radius: 10px;
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+        background-color:rgba(80, 75, 85, .5);
+    }
+    
 </style>
